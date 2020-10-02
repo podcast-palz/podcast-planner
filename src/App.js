63fd9b47fca2f119a03b3a7610710720b619a7d1
pfreemanc <pfreemanc@gmail.com>
2020-10-02 18:56:15 -0400

    Comments

--- src/App.js
+++ src/App.js
@@ -13,8 +13,7 @@ class App extends Component {
 		};
 	}
 
-
-
+  // retrieving the genres, storing them in state. 
 	componentDidMount() {
 		getPodcasts("genres", {top_level_only: 1}).then(response => {
 			console.log(response);
@@ -24,7 +23,7 @@ class App extends Component {
 			})
 		})
 
-
+    // retrieving podcasts with api call from passed params. storing results in state.
 		getPodcasts("best_podcasts", {genre_id: this.state.genre}).then(response => {
 			console.log(response.data.podcasts);
 			this.setState({
