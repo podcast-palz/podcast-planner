3e0e5fa7980cfcae6546bde67674e181e704a952
Nathan Kanigsberg <nathan@kanigsberg.com>
2020-10-02 20:26:16 -0400

    add genre select and submit, time slider

--- src/listenApi.js
+++ src/listenApi.js
@@ -1,14 +1,14 @@
 import Axios from 'axios';
 
-
 // listen API
 const key = "5df881c89d644904bc39af8b922c9441";
 const endpoint = "https://listen-api.listennotes.com/api/v2/";
 
+
 // api call. passing objects that get destructured and passed to the call.
-export async function getPodcasts(requestType, params) {
+export default function listenApi(requestType, params) {
 	console.log('params', params);
-	return await Axios({
+	return Axios({
 		url: endpoint + requestType,
 		params: {
 			...params,
