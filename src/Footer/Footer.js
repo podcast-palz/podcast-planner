e1d97f3cb131829e89c53f8a8ab76556797cb34e
Alex <jalexgould@gmail.com>
2020-10-06 15:17:41 -0400

    footer auto scroll & matching header gradient

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import {VerticleButton as ScrollUpButton} from "react-scroll-up-button";
 import "./index.css";
 
 class Footer extends Component {
    render() {
       return(
-            <footer className="footer">
+            <footer>
+               <div className="footerBar">
                <div className="wrapper container">
-                  <div className="containerDiv">
-                        <p> Podcast Planner!  Start Lisening Today </p>
-                        <p>Podcast Palz Made at Juno </p>
+                  <div>
+                  <ScrollUpButton
+                     StopPosition={0}
+                     ShowAtPosition={130}
+                     EasingType='easeOutCubic'
+                     AnimationDuration={500}
+                     ContainerClassName='ScrollUpButton__Container'
+                     TransitionClassName='ScrollUpButton__Toggled'
+                     style={{}}
+                     ToggledStyle={{}}
+                  /> 
                   </div>
+                  <div className="containerDiv">
+                     <p> Podcast Planner!  Start Lisening Today </p>
+                     <p className="junoMade">Podcast Palz Made at Juno </p>
+                  </div>
+               </div>
                </div>
             </footer>
       )
